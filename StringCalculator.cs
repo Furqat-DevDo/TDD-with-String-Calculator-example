7a0805a7eabd3c223a8f93f9c1d6a0aa99bdcdd5
Furqat-DevDo <furqata93@gmail.com>
2022-06-17 19:49:06 +0500

    TDD example with simple string calculator class

---
+++ StringCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    public  class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (String.IsNullOrEmpty(numbers)) return 0;
+
+            bool IsAllDigits(string s)
+            {
+                foreach (char c in s)
+                {
+                    if (!char.IsDigit(c) && c!=' '&& c!= ',')
+                        return false;
+                }
+                return true;
+            }
+
+            if (IsAllDigits(numbers))
+            {
+                int sum = numbers.Split(new char[] { ',', ' ' })
+                       .Select(n => int.Parse(n))
+                       .Sum();
+                return sum;
+            }
+            else
+            {
+                throw new ArgumentException(nameof(numbers));
+            }
+        }
+    }
+}
